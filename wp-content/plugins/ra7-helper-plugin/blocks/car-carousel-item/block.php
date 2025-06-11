c0e740ade8656117347a25fae3416d0b4d2580aa
Shane Jones <me@shanejones.co.uk>
2025-06-11 21:43:36 +0100

    cars carousel fixed

--- wp-content/plugins/ra7-helper-plugin/blocks/car-carousel-item/block.php
+++ wp-content/plugins/ra7-helper-plugin/blocks/car-carousel-item/block.php
@@ -3,18 +3,19 @@ $link = block_field('link', false);
 $image_id = block_field('image', false); 
 $title = block_field('title', false);
 $button_text = block_field('button-text', false);
+$image = '';
 
 if ($image_id) {
-    $image = wp_get_attachment_image_src($image_id, 'medium')[0];
+    $image_details = wp_get_attachment_image_src($image_id, 'medium');
+    if ($image_details) {
+        $image = $image_details[0];
+    }
 }
 ?>
 
 
-<li class="ras-hp-car-carousel-item">
-  <a href="<?php echo $link; ?>" target="_blank">
-    <div class="ras-hp-car-carousel-item-image">
-      <img src="<?php echo $image; ?>" alt="<?php echo $title; ?>" />
-    </div>
+<li class="ras-hp-car-carousel-item" role="img" aria-label="<?php echo $title; ?>">
+  <a href="<?php echo $link; ?>" target="_blank"  style="background-image: url('<?php echo $image; ?>');">
     <div class="ras-hp-car-carousel-item-content">
       <p><?php echo $title; ?></p>
     </div>
--- wp-content/plugins/ra7-helper-plugin/blocks/car-carousel/block.php
+++ wp-content/plugins/ra7-helper-plugin/blocks/car-carousel/block.php
@@ -7,6 +7,78 @@ if (!wp_script_is('lightslider-js', 'enqueued')) {
     wp_enqueue_script('lightslider-js', plugins_url('../../scripts/lightslider/lightslider.min.js', __FILE__), array('jquery'), '1.1.6', true);
 }
 
+wp_add_inline_script('lightslider-js', '
+  jQuery(document).ready(function($) {
+    var slider;
+    var sliderOptions = {
+      item: 4,
+      slideMove: 1,
+      loop: true,
+      centerMode: false,
+      pager: true,
+      controls: true,
+      enableDrag: true,
+      enableTouch: true,
+      responsive: [
+        {
+          breakpoint: 960,
+          settings: {
+            item: 2,
+            enableDrag: true,
+            enableTouch: true,
+            controls: true,
+            pager: true,
+            loop: true,
+            centerMode: false
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            item: 1,
+            enableDrag: true,
+            enableTouch: true,
+            controls: true,
+            pager: true,
+            loop: true,
+            centerMode: true,
+            centerPadding: "10%"
+          }
+        }
+      ],
+      onSliderLoad: function(el) {
+        if ($(window).width() <= 480) {
+          el.closest(".ras-hp-car-carousel").addClass("slider-active");
+        } else {
+          el.closest(".ras-hp-car-carousel").removeClass("slider-active");
+        }
+      },
+      onResponsiveBreak: function(el) {
+        if ($(window).width() <= 480) {
+          el.closest(".ras-hp-car-carousel").addClass("slider-active");
+        } else {
+          el.closest(".ras-hp-car-carousel").removeClass("slider-active");
+        }
+      }
+    };
+
+    function reinitSlider() {
+      if(slider) {
+          slider.destroy();
+      }
+      slider = $(".ras-hp-car-carousel-slider").lightSlider(sliderOptions);
+    }
+    
+    reinitSlider();
+
+    var resizeTimer;
+    $(window).on("resize", function() {
+      clearTimeout(resizeTimer);
+      resizeTimer = setTimeout(reinitSlider, 250);
+    });
+  });
+');
+
 $title = block_field('title', false);
 $description = block_field('description', false);
 
@@ -25,71 +97,11 @@ $description = block_field('description', false);
         <div class="ras-hp-car-carousel-description"><?php echo $description ?></div>
       <?php endif; ?>
       
-    
+      <div class="ras-hp-car-carousel-slider-wrapper">
         <ul class="ras-hp-car-carousel-slider">
           <?php echo block_value('items'); ?>
         </ul>
+      </div>
     </div>
   </div>  
 </div>
-
-<script type="text/javascript">
-  (function($) {
-    $(document).ready(function() {
-      $('.ras-hp-car-carousel-slider').lightSlider({
-        item: 4,
-        slideMove: 1,
-        slideMargin: 10,
-        loop: true,
-        centerMode: false,
-        pager: true,
-        controls: true,
-        enableDrag: true,
-        enableTouch: true,
-        responsive: [
-          {
-            breakpoint: 960,
-            settings: {
-              item: 2,
-              slideMargin: 10,
-              enableDrag: true,
-              enableTouch: true,
-              controls: true,
-              pager: true,
-              loop: true,
-              centerMode: false
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              item: 1,
-              slideMargin: 10,
-              enableDrag: true,
-              enableTouch: true,
-              controls: true,
-              pager: true,
-              loop: true,
-              centerMode: true,
-              centerPadding: '10%'
-            }
-          }
-        ],
-        onSliderLoad: function(el) {
-          if ($(window).width() <= 480) {
-            el.closest('.ras-hp-car-carousel').addClass('slider-active');
-          } else {
-            el.closest('.ras-hp-car-carousel').removeClass('slider-active');
-          }
-        },
-        onResponsiveBreak: function(el) {
-          if ($(window).width() <= 480) {
-            el.closest('.ras-hp-car-carousel').addClass('slider-active');
-          } else {
-            el.closest('.ras-hp-car-carousel').removeClass('slider-active');
-          }
-        }
-      });
-    });
-  })(jQuery);
-</script>
